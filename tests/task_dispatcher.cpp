6100b8e04a32ac3a7f72bd145fc577a4b6bb2f67
vovs <vgovseychuk@gmail.com>
2026-07-02 20:27:48 +0000

    dispatcher tests

--- tests/task_dispatcher.cpp
+++ tests/task_dispatcher.cpp
@@ -1,7 +1,61 @@
 #include <gtest/gtest.h>
 
+#include <array>
+#include <thread>
+#include <vector>
+
 #include "task_dispatcher.hpp"
 
-TEST(BasicCheck, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+using namespace dispatcher;
 
-// здесь ваш код
+TEST(TaskDispatcherCheck, SingleThreadSimple) {
+    constexpr int CNT = 10;
+    std::array<int, CNT> arr{};
+
+    {
+        TaskDispatcher td{1};
+        for (int i = 0; i < CNT; ++i) {
+            td.schedule(TaskPriority::Normal, [&arr, i]() { arr[i] = i; });
+        }
+    }
+
+    for (int i = 0; i < CNT; ++i) {
+        EXPECT_EQ(arr[i], i);
+    }
+}
+
+TEST(TaskDispatcherCheck, SingleThreadPrio) {
+    constexpr int CNT = 10;
+    constexpr int HV = 22;
+    constexpr int NV = 11;
+    std::vector<int> vec;
+    std::mutex mutex;
+    vec.reserve(CNT * 2 + 1);
+
+    {
+        TaskDispatcher td{1};
+
+        td.schedule(TaskPriority::High, []() { std::this_thread::sleep_for(std::chrono::milliseconds(100)); });
+
+        for (int i = 0; i < CNT; ++i) {
+            td.schedule(TaskPriority::Normal, [&vec, &mutex, NV]() {
+                std::lock_guard lock(mutex);
+                vec.emplace_back(NV);
+            });
+        }
+
+        for (int i = 0; i < CNT; ++i) {
+            td.schedule(TaskPriority::High, [&vec, &mutex, HV]() {
+                std::lock_guard lock(mutex);
+                vec.emplace_back(HV);
+            });
+        }
+    }
+
+    for (int i = 0; i < CNT; ++i) {
+        EXPECT_EQ(vec.at(i), HV);
+    }
+    for (int i = CNT; i < CNT * 2; ++i) {
+        EXPECT_EQ(vec.at(i), NV);
+    }
+}
