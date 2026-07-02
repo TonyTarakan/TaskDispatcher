4be529174576c958e38fd33179435640a6793009
vovs <vgovseychuk@gmail.com>
2026-07-02 19:51:17 +0000

    queue tests

--- tests/queue/unbounded_queue.cpp
+++ tests/queue/unbounded_queue.cpp
@@ -2,4 +2,25 @@
 
 #include "queue/unbounded_queue.hpp"
 
-// здесь ваш код
+TEST(UnboundedQueueCheck, EmptyPop) {
+    dispatcher::queue::UnboundedQueue q{0};
+    ASSERT_FALSE(q.try_pop().has_value());
+}
+
+TEST(UnboundedQueueCheck, PushPop) {
+    dispatcher::queue::UnboundedQueue q{0};
+    bool v1 = false;
+    bool v2 = false;
+    q.push([&]() { v1 = true; });
+    q.push([&]() { v2 = true; });
+
+    EXPECT_EQ(v1, false);
+    EXPECT_EQ(v2, false);
+    (*q.try_pop())();
+    EXPECT_EQ(v1, true);
+    EXPECT_EQ(v2, false);
+    (*q.try_pop())();
+    EXPECT_EQ(v1, true);
+    EXPECT_EQ(v2, true);
+    ASSERT_FALSE(q.try_pop().has_value());
+}
